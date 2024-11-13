f3b4c6479322490e6748b04421e6f796c6222276
Васильев Андрей Георгиевич <vasilyev@tochka.com>
2024-11-13 12:22:56 +0500

    ДЗ-3: коллекции

--- hw03-generics/src/main/java/ru/vasilev/otus/hw03/CustomerService.java
+++ hw03-generics/src/main/java/ru/vasilev/otus/hw03/CustomerService.java
@@ -1,21 +1,27 @@
 package ru.vasilev.otus.hw03;
 
-import java.util.Map;
+import java.util.*;
 
-@SuppressWarnings({"java:S1186", "java:S1135", "java:S1172"}) // при выполнении ДЗ эту аннотацию надо удалить
 public class CustomerService {
 
-    // todo: 3. надо реализовать методы этого класса
-    // важно подобрать подходящую Map-у, посмотрите на редко используемые методы, они тут полезны
+    private final TreeMap<Customer, String> map = new TreeMap<>();
 
     public Map.Entry<Customer, String> getSmallest() {
-        // Возможно, чтобы реализовать этот метод, потребуется посмотреть как Map.Entry сделан в jdk
-        return null; // это "заглушка, чтобы скомилировать"
+        Optional<Map.Entry<Customer, String>> optional = map.entrySet().stream()
+                .min(Comparator.comparingLong(x -> x.getKey().getScores()));
+
+        return optional.map(customerStringEntry ->
+                        Map.entry(customerStringEntry.getKey().copy(), customerStringEntry.getValue()))
+                .orElse(null);
     }
 
     public Map.Entry<Customer, String> getNext(Customer customer) {
-        return null; // это "заглушка, чтобы скомилировать"
+        Map.Entry<Customer, String> entry = map.higherEntry(customer);
+
+        return entry != null ? Map.entry(entry.getKey().copy(), entry.getValue()) : null;
     }
 
-    public void add(Customer customer, String data) {}
+    public void add(Customer customer, String data) {
+        map.put(customer.copy(), data);
+    }
 }
