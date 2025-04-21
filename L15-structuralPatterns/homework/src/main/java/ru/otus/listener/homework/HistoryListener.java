f9af201f14568b6e9c467e4d71d1085824221022
Васильев Андрей Георгиевич <vasilyev@tochka.com>
2025-04-21 21:21:37 +0500

    ДЗ-15: реализован HistoryListenerTest

--- L15-structuralPatterns/homework/src/main/java/ru/otus/listener/homework/HistoryListener.java
+++ L15-structuralPatterns/homework/src/main/java/ru/otus/listener/homework/HistoryListener.java
@@ -1,18 +1,22 @@
 package ru.otus.listener.homework;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 import ru.otus.listener.Listener;
 import ru.otus.model.Message;
 
 public class HistoryListener implements Listener, HistoryReader {
 
+    private Map<Long, Message> messages = new HashMap<>();
+
     @Override
     public void onUpdated(Message msg) {
-        throw new UnsupportedOperationException();
+        messages.put(msg.getId(), msg.toBuilder().build());
     }
 
     @Override
     public Optional<Message> findMessageById(long id) {
-        throw new UnsupportedOperationException();
+        return Optional.ofNullable(messages.get(id));
     }
 }
--- L15-structuralPatterns/homework/src/main/java/ru/otus/model/Message.java
+++ L15-structuralPatterns/homework/src/main/java/ru/otus/model/Message.java
@@ -1,20 +1,35 @@
 package ru.otus.model;
 
-@SuppressWarnings({"java:S107", "java:S1135"})
+@SuppressWarnings({ "java:S107", "java:S1135" })
 public class Message {
+
     private final long id;
+
     private final String field1;
+
     private final String field2;
+
     private final String field3;
+
     private final String field4;
+
     private final String field5;
+
     private final String field6;
+
     private final String field7;
+
     private final String field8;
+
     private final String field9;
+
     private final String field10;
 
-    // todo: 1. Добавить поля field11 - field13 (для field13 используйте класс ObjectForMessage)
+    private final String field11;
+
+    private final String field12;
+
+    private final ObjectForMessage field13;
 
     private Message(
             long id,
@@ -27,7 +42,12 @@ public class Message {
             String field7,
             String field8,
             String field9,
-            String field10) {
+            String field10,
+            String field11,
+            String field12,
+            ObjectForMessage field13
+    ) {
+
         this.id = id;
         this.field1 = field1;
         this.field2 = field2;
@@ -39,45 +59,58 @@ public class Message {
         this.field8 = field8;
         this.field9 = field9;
         this.field10 = field10;
+        this.field11 = field11;
+        this.field12 = field12;
+        this.field13 = field13;
     }
 
     public long getId() {
+
         return id;
     }
 
     public String getField1() {
+
         return field1;
     }
 
     public String getField2() {
+
         return field2;
     }
 
     public String getField3() {
+
         return field3;
     }
 
     public String getField4() {
+
         return field4;
     }
 
     public String getField5() {
+
         return field5;
     }
 
     public String getField6() {
+
         return field6;
     }
 
     public String getField7() {
+
         return field7;
     }
 
     public String getField8() {
+
         return field8;
     }
 
     public String getField9() {
+
         return field9;
     }
 
@@ -85,10 +118,27 @@ public class Message {
         return field10;
     }
 
+    public String getField11() {
+        return field11;
+    }
+
+    public String getField12() {
+        return field12;
+    }
+
+    public ObjectForMessage getField13() {
+        return field13;
+    }
+
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
 
         Message message = (Message) o;
 
@@ -97,15 +147,18 @@ public class Message {
 
     @Override
     public int hashCode() {
+
         return (int) (id ^ (id >>> 32));
     }
 
     public Builder toBuilder() {
-        return new Builder(id, field1, field2, field3, field4, field5, field6, field7, field8, field9, field10);
+
+        return new Builder(id, field1, field2, field3, field4, field5, field6, field7, field8, field9, field10, field11, field12, field13);
     }
 
     @Override
     public String toString() {
+
         return "Message{" + "id="
                 + id + ", field1='"
                 + field1 + '\'' + ", field2='"
@@ -117,23 +170,44 @@ public class Message {
                 + field7 + '\'' + ", field8='"
                 + field8 + '\'' + ", field9='"
                 + field9 + '\'' + ", field10='"
-                + field10 + '\'' + '}';
+                + field10 + '\'' + ", field11='"
+                + field11 + '\'' + ", field12='"
+                + field12 + '\'' + ", field13='"
+                + field13 + '\'' + '}';
     }
 
     public static class Builder {
+
         private final long id;
+
         private String field1;
+
         private String field2;
+
         private String field3;
+
         private String field4;
+
         private String field5;
+
         private String field6;
+
         private String field7;
+
         private String field8;
+
         private String field9;
+
         private String field10;
 
+        private String field11;
+
+        private String field12;
+
+        private ObjectForMessage field13;
+
         public Builder(long id) {
+
             this.id = id;
         }
 
@@ -148,7 +222,12 @@ public class Message {
                 String field7,
                 String field8,
                 String field9,
-                String field10) {
+                String field10,
+                String field11,
+                String field12,
+                ObjectForMessage field13
+        ) {
+
             this.id = id;
             this.field1 = field1;
             this.field2 = field2;
@@ -160,60 +239,92 @@ public class Message {
             this.field8 = field8;
             this.field9 = field9;
             this.field10 = field10;
+            this.field11 = field11;
+            this.field12 = field12;
+            this.field13 = field13.copy();
         }
 
         public Builder field1(String field1) {
+
             this.field1 = field1;
             return this;
         }
 
         public Builder field2(String field2) {
+
             this.field2 = field2;
             return this;
         }
 
         public Builder field3(String field3) {
+
             this.field3 = field3;
             return this;
         }
 
         public Builder field4(String field4) {
+
             this.field4 = field4;
             return this;
         }
 
         public Builder field5(String field5) {
+
             this.field5 = field5;
             return this;
         }
 
         public Builder field6(String field6) {
+
             this.field6 = field6;
             return this;
         }
 
         public Builder field7(String field7) {
+
             this.field7 = field7;
             return this;
         }
 
         public Builder field8(String field8) {
+
             this.field8 = field8;
             return this;
         }
 
         public Builder field9(String field9) {
+
             this.field9 = field9;
             return this;
         }
 
         public Builder field10(String field10) {
+
             this.field10 = field10;
             return this;
         }
 
+        public Builder field11(String field11) {
+
+            this.field11 = field11;
+            return this;
+        }
+
+        public Builder field12(String field12) {
+
+            this.field12 = field12;
+            return this;
+        }
+
+        public Builder field13(ObjectForMessage field13) {
+
+            this.field13 = field13;
+            return this;
+        }
+
         public Message build() {
-            return new Message(id, field1, field2, field3, field4, field5, field6, field7, field8, field9, field10);
+
+            return new Message(id, field1, field2, field3, field4, field5, field6, field7, field8, field9, field10, field11, field12, field13);
         }
     }
 }
