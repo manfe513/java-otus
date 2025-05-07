c37ed7dce19f810dcd797d6daa4d65db250d2144
Васильев Андрей Георгиевич <vasilyev@tochka.com>
2025-05-07 19:46:47 +0500

    ДЗ-18: DataTemplateJdbc

--- L18-jdbc/homework/src/main/java/ru/otus/jdbc/mapper/DataTemplateJdbc.java
+++ L18-jdbc/homework/src/main/java/ru/otus/jdbc/mapper/DataTemplateJdbc.java
@@ -1,40 +1,114 @@
 package ru.otus.jdbc.mapper;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+
 import ru.otus.core.repository.DataTemplate;
 import ru.otus.core.repository.executor.DbExecutor;
 
-/** Сохратяет объект в базу, читает объект из базы */
+/**
+ * Сохратяет объект в базу, читает объект из базы
+ */
 @SuppressWarnings("java:S1068")
 public class DataTemplateJdbc<T> implements DataTemplate<T> {
 
     private final DbExecutor dbExecutor;
+
     private final EntitySQLMetaData entitySQLMetaData;
 
-    public DataTemplateJdbc(DbExecutor dbExecutor, EntitySQLMetaData entitySQLMetaData) {
+    private final EntityClassMetaData<T> entityClassMetaData;
+
+    public DataTemplateJdbc(DbExecutor dbExecutor,
+            EntitySQLMetaData entitySQLMetaData,
+            EntityClassMetaData<T> entityClassMetaData) {
+
         this.dbExecutor = dbExecutor;
         this.entitySQLMetaData = entitySQLMetaData;
+        this.entityClassMetaData = entityClassMetaData;
     }
 
     @Override
     public Optional<T> findById(Connection connection, long id) {
-        throw new UnsupportedOperationException();
+
+        return dbExecutor.executeSelect(connection, entitySQLMetaData.getSelectByIdSql(), List.of(id), this::mapEntity);
+    }
+
+    private T mapEntity(ResultSet rs) {
+        try {
+            T obj = entityClassMetaData.getConstructor().newInstance();
+
+            entityClassMetaData.getAllFields()
+                    .forEach(f -> {
+                        f.setAccessible(true);
+
+                        try {
+                            f.set(obj, rs.getObject(f.getName()));
+                        } catch (Exception e) {
+                            throw new RuntimeException(e);
+                        }
+                    });
+
+            return obj;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public List<T> findAll(Connection connection) {
-        throw new UnsupportedOperationException();
+
+        return dbExecutor.executeSelect(connection, entitySQLMetaData.getSelectAllSql(), List.of(), rs -> {
+
+            List<T> items = new ArrayList<>();
+
+            try {
+                do {
+                    mapEntity(rs);
+                } while (rs.next());
+
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+
+            return items;
+        }).orElse(new ArrayList<>());
     }
 
     @Override
     public long insert(Connection connection, T client) {
-        throw new UnsupportedOperationException();
+        List<Object> values = fieldsToValues(entityClassMetaData.getAllFields(), client);
+
+        return dbExecutor.executeStatement(connection, entitySQLMetaData.getInsertSql(), values);
+    }
+
+    private List<Object> fieldsToValues(List<Field> fields, T client) {
+        return fields.stream()
+                .map(f -> {
+                    f.setAccessible(true);
+                    try {
+                        return f.get(client);
+                    } catch (IllegalAccessException e) {
+                        throw new RuntimeException(e);
+                    }
+                }).toList();
     }
 
     @Override
     public void update(Connection connection, T client) {
-        throw new UnsupportedOperationException();
+        List<Object> values = fieldsToValues(entityClassMetaData.getFieldsWithoutId(), client);
+
+        try {
+            values.add(entityClassMetaData.getIdField().get(client));
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+
+        dbExecutor.executeStatement(connection, entitySQLMetaData.getUpdateSql(), values);
     }
 }
